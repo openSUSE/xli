3c257b8a861e96f008f057d9f755a58357d56a8f
Tomáš Chvátal <tchvatal@suse.com>
2017-07-26 10:37:07 +0200

    Check png signatures using png_sig_cmp

--- png.c
+++ png.c
@@ -11,7 +11,7 @@
 
 #define TITLE_KEYWORD "Title"
 
-/* check to see if a file is a png file using png_check_sig() */
+/* check to see if a file is a png file using png_sig_cmp() */
 static int check_png(char *file_name)
 {
 	ZFILE *zfp;
@@ -27,7 +27,7 @@ static int check_png(char *file_name)
 	if (ret != 8)
 		return 0;
 
-	ret = png_check_sig(buf, 8);
+	ret = !png_sig_cmp(buf, 0, 8);
 
 	return (ret);
 }
